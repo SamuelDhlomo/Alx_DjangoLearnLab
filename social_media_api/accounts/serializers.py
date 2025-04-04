e3e7589de94b541901fbd4573706e086265ab66d
Samuel Dhlomo <samuel.o.dhlomo@gmail.com>
2025-04-04 23:59:06 +0200

    Commiting changes to user registartion and login

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,37 +1,19 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
-from .models import CustomUser  # Make sure to import your CustomUser model
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, min_length=8, style={'input_type': 'password'})
-    password2 = serializers.CharField(write_only=True, min_length=8, style={'input_type': 'password'})
-
     class Meta:
-        model = CustomUser
-        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers', 'password', 'password2']
-        extra_kwargs = {'password': {'write_only': True}, 'password2': {'write_only': True}}
-
-    def validate(self, data):
-        if data['password'] != data['password2']:
-            raise serializers.ValidationError("Passwords do not match.")
-        return data
+        model = get_user_model()
+        fields = ['username', 'password', 'email', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        user = get_user_model().objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password']
-        )
-        # You might not want to include 'bio', 'profile_picture', 'followers' during registration
-        # If you do, you can set them here:
-        if 'bio' in validated_data:
-            user.bio = validated_data['bio']
-        if 'profile_picture' in validated_data:
-            user.profile_picture = validated_data['profile_picture']
-        # Followers should typically not be set during registration
-        user.save()
-
-        # Create a token for the new user
-        token, created = Token.objects.get_or_create(user=user)
+        user = get_user_model().objects.create_user(**validated_data)
+        # Generate a token when a new user is created
+        Token.objects.create(user=user)
         return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
